fccb0c7933fbbd5874ed515c1f59102a7a5bd037
oliverLo78 <oliverberto@gmail.com>
2022-12-03 17:15:20 -0500

    recreated react-portfolio app

---
+++ react-portfolio/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// ReactDom library from react 17 
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Here we import bootstrap for access to some additional styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
