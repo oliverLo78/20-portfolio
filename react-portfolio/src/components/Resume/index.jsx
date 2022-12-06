079419561c322941b34324687edb080a6305d1cd
oliverLo78 <oliverberto@gmail.com>
2022-12-05 22:48:50 -0500

    added resume div with front end skills

---
+++ react-portfolio/src/components/Resume/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ResumeDiv() {
+  return (
+    <div>
+      <h1>Download my resume</h1>
+      <h2>Front-end Proficiencies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ResumeDiv;
