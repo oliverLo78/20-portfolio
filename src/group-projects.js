e3601c006f4ca2268e822194dd3b34e6ef1ea908
oliverLo78 <oliverberto@gmail.com>
2023-02-27 18:31:01 -0500

    added group-projects array

--- src/group-projects.js
+++ src/group-projects.js
@@ -1,16 +1,36 @@
 
-import speakeasy from './images/shocase.PNG'
-import gamehub from './images/gamehub.PNG'
-import uspostalprinting from './images/us-postal-printing.PNG'
+import speakeasy from './images/Speakeasy-Screenshot.png';
+
 
 const GroupProjects = [
     {
         id: 1,
         title: 'SpeakEasy',
-        description: 'SpeakEase is a full stack web application that serves as a social media platform for sharing anime reccomendations through a playlist format. The user is able to make playlists of 4 animes, comment on other users playlists, and save playlists within their liked page. The project utilizes an Express backend, RESTFUL api, mySQL database, and a third party API.',
-        imagePath: shocase,
+        description: 'SpeakEasy project Discover classic cocktails or get information on you favorite one. See their ingredients and bio and also watch Youtube tutorials on creating them.',
+        imagePath: speakeasy,
         imageAlt: 'Image of SpeakEasy App',
         link: 'https://jkanvision.github.io/The-SpeakEasy-Project/',
         github: 'https://github.com/jkanvision/The-SpeakEasy-Project.git',
         technologies: 'HTML5, CSS3, JavaScript, jQuery, Bulma'
     },
+    {
+        id: 1,
+        title: 'SpeakEasy',
+        description: 'SpeakEasy project Discover classic cocktails or get information on you favorite one. See their ingredients and bio and also watch Youtube tutorials on creating them.',
+        imagePath: speakeasy,
+        imageAlt: 'Image of SpeakEasy App',
+        link: 'https://jkanvision.github.io/The-SpeakEasy-Project/',
+        github: 'https://github.com/jkanvision/The-SpeakEasy-Project.git',
+        technologies: 'HTML5, CSS3, JavaScript, jQuery, Bulma'
+    },
+    {
+        id: 1,
+        title: 'SpeakEasy',
+        description: 'SpeakEasy project Discover classic cocktails or get information on you favorite one. See their ingredients and bio and also watch Youtube tutorials on creating them.',
+        imagePath: speakeasy,
+        imageAlt: 'Image of SpeakEasy App',
+        link: 'https://jkanvision.github.io/The-SpeakEasy-Project/',
+        github: 'https://github.com/jkanvision/The-SpeakEasy-Project.git',
+        technologies: 'HTML5, CSS3, JavaScript, jQuery, Bulma'
+    },
+]
