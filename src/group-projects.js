0b3062608aff6a08d673ea21ca927904851f8584
oliverLo78 <oliverberto@gmail.com>
2023-02-27 18:26:42 -0500

    added images folder and fav pictures from recent trips

---
+++ src/group-projects.js
@@ -0,0 +1,16 @@
+
+import speakeasy from './images/shocase.PNG'
+import gamehub from './images/gamehub.PNG'
+import uspostalprinting from './images/us-postal-printing.PNG'
+
+const GroupProjects = [
+    {
+        id: 1,
+        title: 'SpeakEasy',
+        description: 'SpeakEase is a full stack web application that serves as a social media platform for sharing anime reccomendations through a playlist format. The user is able to make playlists of 4 animes, comment on other users playlists, and save playlists within their liked page. The project utilizes an Express backend, RESTFUL api, mySQL database, and a third party API.',
+        imagePath: shocase,
+        imageAlt: 'Image of SpeakEasy App',
+        link: 'https://jkanvision.github.io/The-SpeakEasy-Project/',
+        github: 'https://github.com/jkanvision/The-SpeakEasy-Project.git',
+        technologies: 'HTML5, CSS3, JavaScript, jQuery, Bulma'
+    },
