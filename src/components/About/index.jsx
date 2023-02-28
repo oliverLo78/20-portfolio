64c4a9c2f4b34b1ede68a194905cc057260438a8
oliverLo78 <oliverberto@gmail.com>
2023-02-28 07:04:44 -0500

    changed contact and about components

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,97 +1,23 @@
 import React, { useState } from 'react';
 
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from 'reactstrap';
+import headshot from '/src/images/oliver ProfilePic-sm.jpg'
 
-const items = [
-  {
-    src: 'https://picsum.photos/id/123/1900/800',
-    altText: 'Slide 1',
-    caption: 'Hi my name is Oliver  Ready to embark on the journey of Software Engineer. I enjoy creating Websites and software applications to improve the end user experience. Very excited to be a part of UCF coding bootcamp. A catalyst with plenty of energy in learning HTML & CSS and Git, JavaScript & Web APIs, Node.js',
-    key: 1,
-  },
-  {
-    src: 'https://picsum.photos/id/456/1900/800',
-    altText: 'Slide 2',
-    caption: ' With each project, I aim to understand how best to engage users for an impactful user experience while keeping stakeholder initiatives in mind. I practice fiscal responsibility to optimize business solutions and product outcomes.',
-    key: 2,
-  },
-  {
-    src: 'https://picsum.photos/id/678/1900/800',
-    altText: 'Slide 3',
-    caption: 'When I’m not coordinating projects and promoting efficiency, I enjoy visiting beautiful islands like Punta Cana and Bora Bora, trying out new recipes, or honing my skills with new courses.',
-    key: 3,
-  },
-];
-
-function About(args) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  };
-
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  };
-
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  };
-
-  const slides = items.map((item) => {
+function About() {
     return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <img src={item.src} alt={item.altText} />
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        />
-      </CarouselItem>
-    );
-  });
-
-  return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-      {...args}
-    >
-      <CarouselIndicators
-        items={items}
-        activeIndex={activeIndex}
-        onClickHandler={goToIndex}
-      />
-      {slides}
-      <CarouselControl
-        direction="prev"
-        directionText="Previous"
-        onClickHandler={previous}
-      />
-      <CarouselControl
-        direction="next"
-        directionText="Next"
-        onClickHandler={next}
-      />
-    </Carousel>
-  );
-}
+      <div className="about-container">
+          <div className="left-side">
+          <h1 style={{fontSize: '60px'}}>About Me!</h1>
+          <p className="mt-5" style={{width: '80%', fontSize: '20px'}}>
+          Hi my name is Oliver  Ready to embark on the journey of Software Engineer. I enjoy creating Websites and software applications to improve the end user experience. Very excited to be a part of UCF coding bootcamp. A catalyst with plenty of energy in learning HTML & CSS and Git, JavaScript & Web APIs, Node.js</p>
+          <br></br>
+          <p className='mt-5' style={{width: '80%', fontSize: '20px'}}>
+          When I’m not coordinating projects and promoting efficiency, I enjoy visiting beautiful islands like Punta Cana, Bora Bora, trying out new recipes, or honing my skills with new courses.</p>
+          <br></br>
+                  </div>
+          <img className="headshot" src={headshot} alt="Oliver Lopez"></img>
+      </div>
+      )
+  }
 
 export default About;
 
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,118 +1,67 @@
-// import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 // import Contact from './components/utils';
-// import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
+import  Modal from 'react-bootstrap/Modal';
 
 // Here we import a helper function that will check if the email is valid
 // import { checkMessage, validateEmail } from '../../utils/helpers';
 
-// function Contact() {
+ function Contact() {
   // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [message, setMessage] = useState('');
-  // const [errorMessage, setErrorMessage] = useState('');
-  // const form = useRef();
+  // We are also setting their initial values to false
+  const [email, setEmail] = useState('false');
+
+  const messageClose = () => setEmail(false); 
+  const messageOpen = () => setEmail(true);
   
-  // const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    // const { target } = e;
-    // const inputType = target.name;
-    // const inputValue = target.value;
+   const form = useRef();
+  
+   const sendEmail = (e) => {
+      e.preventDefault();
 
-    // Based on the input type, we set the state of either email, username, and password
-  //   if (inputType === 'name') {
-  //     setName(inputValue);
-  //   } else if (inputType === 'Email') {
-  //     setEmail(inputValue);
-  //   }  else if (inputType === 'message') {
-  //       setMessage(inputValue);
-  //   } else {
-  //     errorMessage('Message required...');
-  //   }
-  // };
-
-  // const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    // e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    // if (!validateEmail(email) || !name) {
-    //   setErrorMessage('Email or name is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      // return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    // }
-    // if (!checkMessage(message)) {
-    //   setErrorMessage(
-    //     `Message required for your account: ${name}`
-    //   );
-    //   return;
-    // }
+      emailjs.sendForm('service_yxf1m8j', 'template_92yyfem', form.current, 'RQhNo667QGSlWoBNi')
+       .then((result) => {
+         console.log(result.text);
+         messageOpen();
+         e.target.reset();
+     }, (error) => {
+         console.log(error.text);
+     });
+ }
+  
+  return (
+    <div className="contact-container">
+      <div className="contact-wrapper">
+      <h1 style={{ fontSize: '60px'}}>Contact Me!</h1>
+    <form className="contact-form" ref={form} onSubmit={sendEmail}>
+        <div className='top-form'>
+        <i className='fa-solid fa-envelope custom-envelope'></i>
+        <div className='top-form-inputs'>
+          <label className="form-label">Name:</label>
+          <input type="text" name="user-name" className='text-inputs' required />
+          <label className="form-label">Email Address:</label>
+          <input type="email" name="user-email" className='text-inputs' required />
+        </div>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', width: '100%'}}>
+      <label className='form-label'>Message</label>
+      <textarea className='message-text' name="message" required />
+        <input className='send-btn' type="submit" value="Send" />
+         <label class="form-label">Message:</label>
+      </div>
+    </form>
     
-      // export const ContactUs = () => {
-     
-    
-  //     const sendEmail = (e) => {
-  //       e.preventDefault();
-  //   emailjs.sendForm('service_yxf1m8j', 'template_92yyfem', form.current, 'RQhNo667QGSlWoBNi')
-  //   .then((result) => {
-  //       console.log(result.text);
-  //   }, (error) => {
-  //       console.log(error.text);
-  //   });
+    <Modal email={show} onHide={messageClose}>
+      <Modal.Header closeButton style={{ backgroundColor: '#cbfff5', borderRadius: '5px' }}>
+          <Modal.Title>Message Sent!</Modal.Title>
+      </Modal.Header>
+    </Modal>
+    </div>
+  </div>
+  )
+}
 
-    //  alert(`Hello ${name}`);
-  
-  //  If everything goes according to plan, we want to clear out the input after a successful registration.
-  //   setName('');
-  //   setEmail('');
-  //   setMessage('');
-  // };
-  // }
-  
-  // return (
-  //   <div class="col-md-6">
-  //     <p>Hello {name}</p>
-  //     <form className="form" ref={form} >
-  //       <label class="form-label">Name:</label>
-  //       <input
-  //         value={name}
-  //         name="name"
-  //         onChange={handleInputChange}
-  //         type="name"
-  //         class="form-control"
-  //         placeholder="name"
-  //       />
-  //        <label class="form-label">Email Address:</label>
-  //       <input
-  //         value={email}
-  //         name="email"
-  //         onChange={handleInputChange}
-  //         type="email"
-  //         class="form-control"
-  //         placeholder="email"
-  //       />
-  //        <label class="form-label">Message:</label>
-  //       <textarea
-  //         value={message}
-  //         name="message"
-  //         onChange={handleInputChange}
-  //         type="message"
-  //         class="form-control"
-  //         placeholder="Add a Message here"
-  //       />
-  //       <button class="form-control" type="button" onClick={handleFormSubmit}>Submit</button>
-  //     </form>
-  //     {errorMessage && (
-  //       <div>
-  //         <p className="error-text">{errorMessage}</p>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-
-  // }
+export default Contact;
 
 /******************************************************************************************************************************* */
 // import React from 'react';
