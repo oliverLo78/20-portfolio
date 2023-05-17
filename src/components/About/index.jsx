5cc32aa9c9f78cae1a5bf7ab94691a8d8dacb7bf
oliverLo78 <oliverberto@gmail.com>
2023-05-17 17:50:04 -0400

    added brand statement

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,8 +10,8 @@ function About() {
           <h1 style={{fontSize: '60px'}}>About Me!</h1>
           <p className="mt-5" style={{width: '80%', fontSize: '20px'}}>
           Hi my name is Oliver  Ready to embark on the journey of Software Engineer. I enjoy creating Websites and software applications to improve the end user experience. Very excited to be a part of UCF coding bootcamp. A catalyst with plenty of energy in learning HTML & CSS and Git, JavaScript & Web APIs, Node.js.<br></br><br></br>
-          When I’m not coordinating projects and promoting efficiency, I enjoy visiting beautiful islands like Punta Cana, Bora Bora, trying out new recipes, and working at honing my skills with new coding courses.<br></br><br></br>
-          I am very excited of being involved in the tech industry as a Web Developer and would jump at the opportunity to be a part of a company that shares similar values.</p><br></br>
+          When I’m not coordinating projects and promoting efficiency, I enjoy visiting beautiful islands like Punta Cana, Bora Bora, trying out new recipes, and training on Gracie Jiu-Jitsu for self delf.<br></br><br></br>
+          Project manager with leadership experience and technical know-how. Previous experience includes project manager for a general contractor company in Winter Garden FL,  ANC Roofing, where I oversaw the execution of weekly re-roofs and managed a team of seven-five crew members. Additionally, I completed the Web Developer Certificate at the University of Central Florida, where I developed knowledge and experience in Scrum and Agile frameworks as well as Software and Project Management lifecycles, among others. Planned, developed, and successfully executed projects, deployed by Heroku, and have led teams in previous roles of varying size to meet company goals. Excited to combine technical training and management experience to continue leading teams to success..</p><br></br>
           </div>
       </div>
       )
