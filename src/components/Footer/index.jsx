f5e0a56f1d83e3e14eb9a9584a3bd0dd0f0160c6
oliverLo78 <oliverberto@gmail.com>
2023-05-18 14:27:39 -0400

    fixing footer and adding link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,27 +3,22 @@ import './footer.css'
 
 const Footer = () => {
   return ( 
-    <div className='footer-container'>
-        <ul className='footer-ul'>
+    <div className='container-fluid d-flex justify-content-between footer-background'>
+        <ul className='footer-container'>
           <li className='footer-li'><a href='https://twitter.com' target='_blank' rel='noreferrer'>
-            <i className='fa-brands fa-twitter'></i></a>
+            <i className='fa fa-twitter'></i></a>
           </li>
           <li className='footer-li'><a href='https://github.com' target='_blank' rel='noreferrer'>
-            <i className='fa-brands fa-github'></i>
+            <i className='fa fa-github'></i>
           </a></li>
-          <li className='footer-li'><a href='https://linkedin.com' target='_blank' rel='noreferrer'>
-            <i className='fa-brands fa-linkedin'></i>
+          <li className='footer-li'><a href='https://www.linkedin.com/in/oliver-lopez78/' target='_blank' rel='noreferrer'>
+            <i className='fa fa-linkedin'></i>
           </a></li>
           <li style={{display: 'flex', alignItems: 'center', cursor: 'auto'}} className='footer-li'>
             <a href= "mailto: 'oliverberto@gmail.com'" target='_blank' rel='noreferrer'>
-              <i className='fa-solid fa-envelope'></i>
-            </a>
-            <span className='hide' style={{ cursor: 'auto', fontSize: '20px', textDecoration: 'none', color: '#000', paddingLeft: '10px'}}>
-              oliverberto@gmail.com
-            </span>
-          </li>
+              <i className='fa-solid fa-envelope'></i></a><span className='hide' style={{ cursor: 'auto', fontSize: '20px', textDecoration: 'none', color: '#000', paddingLeft: '10px' }}>oliverberto@gmail.com</span></li>
         </ul>
-    </div>
+      </div>
   );
 }
 
