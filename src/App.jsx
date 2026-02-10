38bbba24e334af6a836c68f42d799d0ddd8f7932
Oliver Lopez <oliverberto@gmail.com>
2026-02-09 21:43:22 -0500

    removed Skills prop from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,17 @@ import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Skills from "./components/Skills";
 
 const pages = [
   { bg: "prj-one", component: About },
   { bg: "prj-two", component: Portfolio },
   { bg: "prj-three", component: Resume },
-  { bg: "prj-four", component: Skills },
   { bg: "prj-five", component: Contact },
 ];
 
 const backgrounds = [
   "/images/trip-san-francisco.jpg",     // About Me
   "/images/trip-san-francisco-2.jpg",   // Portfolio
-  "/images/trip-san-francisco.jpg",   // Resume
   "/images/muir-woods.jpg",             // Resume
   "/images/atl-aquarium-2.jpg",             // Contact
 ];
@@ -53,8 +50,7 @@ export default function App() {
         showAbout={() => setPageIndex(0)}
         showProjects={() => setPageIndex(1)}
         showResume={() => setPageIndex(2)}
-        showSkills={() => setPageIndex(3)}
-        showContact={() => setPageIndex(4)}
+        showContact={() => setPageIndex(3)}
       />
 
       {/* Main content grows to fill space */}
